b5f9ea5f0c2afbdf4e4b296927374f011c7c7242
hortencia c <hortenciaecisneros@gmail.com>
2020-07-20 13:55:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -15,6 +15,6 @@ end
 
 
 def id_hash_generator(number)
-  number = {:id_hash_generator => Integer}
+  number = {:id_hash_generator(5) => Integer }
   # return a hash with a key :id assigned to the provided number
 end
